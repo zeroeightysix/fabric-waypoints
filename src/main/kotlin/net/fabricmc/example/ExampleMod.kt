c90fb7ad1e0932495522329f33de65cc0537c566
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-04-08 16:01:09 +0200

    Modified gradle & resources

--- src/main/kotlin/net/fabricmc/example/ExampleMod.kt
+++ src/main/kotlin/net/fabricmc/example/ExampleMod.kt
@@ -1,7 +1,5 @@
 package net.fabricmc.example
 
-// For support join https://discord.gg/v6v4pMv
-
 @Suppress("unused")
 fun init() {
     // This code runs as soon as Minecraft is in a mod-load-ready state.
