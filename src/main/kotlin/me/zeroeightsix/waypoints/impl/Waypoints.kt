c9283c2ed4f2f1ee5e8007bc438944dc575e2820
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-04-09 15:54:48 +0200

    Make the mod actually launch

--- src/main/kotlin/me/zeroeightsix/waypoints/impl/Waypoints.kt
+++ src/main/kotlin/me/zeroeightsix/waypoints/impl/Waypoints.kt
@@ -1,11 +1,9 @@
 package me.zeroeightsix.waypoints.impl
 
+import me.zeroeightsix.fiber.builder.ConfigNodeBuilder
+
 @Suppress("unused")
 fun init() {
-    // This code runs as soon as Minecraft is in a mod-load-ready state.
-    // However, some things (like resources) may still be uninitialized.
-    // Proceed with mild caution.
-
-    println("Hello Fabric world!")
+    
 }
 
