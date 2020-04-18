839f12784a6a77dd19bc8e6c76d48b07a7e76b59
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-04-18 12:33:11 +0200

    Add profiler endpoints

--- src/main/java/me/zeroeightsix/waypoints/mixin/MixinGameRenderer.java
+++ src/main/java/me/zeroeightsix/waypoints/mixin/MixinGameRenderer.java
@@ -10,7 +10,9 @@ import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.Matrix4f;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.Vec2f;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -23,6 +25,7 @@ import java.util.stream.Collectors;
 @Mixin(GameRenderer.class)
 public class MixinGameRenderer {
 
+    @Shadow @Final private MinecraftClient client;
     private final WeakHashMap<Waypoint, Vec2f> screenPositions = new WeakHashMap<>();
 
     @Inject(method = "renderWorld",
@@ -31,6 +34,7 @@ public class MixinGameRenderer {
                     target = "Lnet/minecraft/client/render/WorldRenderer;render(Lnet/minecraft/client/util/math/MatrixStack;FJZLnet/minecraft/client/render/Camera;Lnet/minecraft/client/render/GameRenderer;Lnet/minecraft/client/render/LightmapTextureManager;Lnet/minecraft/client/util/math/Matrix4f;)V"
             ), locals = LocalCapture.CAPTURE_FAILHARD)
     public void onRenderWorld(float tickDelta, long limitTime, MatrixStack matrix, CallbackInfo ci, boolean bl, Camera camera, MatrixStack matrixStack, Matrix4f matrix4f) {
+        client.getProfiler().push("waypoints");
         float scale = MinecraftClient.getInstance().getWindow().calculateScaleFactor(
                 MinecraftClient.getInstance().options.guiScale,
                 MinecraftClient.getInstance().forcesUnicodeFont()
@@ -40,13 +44,16 @@ public class MixinGameRenderer {
             Vec2f p = VectorMath.divideVec2f(VectorMath.project3Dto2D(camera.getPos().negate().add(waypoint.getPosition()), matrix.peek().getModel(), matrix4f), scale);
             screenPositions.put(waypoint, p);
         });
+        client.getProfiler().pop();
     }
 
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;render(F)V", shift = At.Shift.AFTER))
     public void onRender(CallbackInfo ci) {
+        client.getProfiler().push("waypoints");
         Waypoints.getAccessor().getWaypoints().forEach((waypointRenderer, waypoints) -> waypointRenderer.render(
                 waypoints.stream().map(waypoint -> new RenderWaypoint(waypoint, screenPositions.remove(waypoint))).collect(Collectors.toSet())
         ));
+        client.getProfiler().pop();
     }
 
 }
